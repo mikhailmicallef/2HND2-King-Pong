61c32094b1f92802cb5719781a6b5e8d7bfbd4ee
mikhailmicallef <micallefmikhail@gmail.com>
2014-02-18 13:22:53 +0100

    Edit Unity Project
    Edit All Files

---
+++ King Pong Game/Assets/Scripts/SphereGenerator.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+var thesphere:Rigidbody;
+//rigid body that links to  to the sphere prefab
+
+var maximumspheres:int;
+//the total number of spheres to generate
+
+function Start () {
+
+}
+
+function Update () {
+
+}
